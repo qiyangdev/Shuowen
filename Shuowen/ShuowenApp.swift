b1e0918d4e97cc78008adb7d204ed1afae9628e7
qiyang <wangqiyangx@gmail.com>
2026-06-12 09:20:00 +0800

    Initial Commit

---
+++ Shuowen/ShuowenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuowenApp.swift
+//  Shuowen
+//
+//  Created by wangqiyang on 2026/6/12.
+//
+
+import SwiftUI
+
+@main
+struct ShuowenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShuowenTests/ShuowenTests.swift
@@ -0,0 +1,18 @@
+//
+//  ShuowenTests.swift
+//  ShuowenTests
+//
+//  Created by wangqiyang on 2026/6/12.
+//
+
+import Testing
+
+struct ShuowenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
